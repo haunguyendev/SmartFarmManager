2a4c920b744a0744d542a8faeea8460dd0496c7d
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-04-20 20:04:13 +0700

    update get farming batches

--- SmartFarmManager.Service/Services/FarmingBatchService.cs
+++ SmartFarmManager.Service/Services/FarmingBatchService.cs
@@ -678,6 +678,8 @@ namespace SmartFarmManager.Service.Services
                     CleaningFrequency = fb.CleaningFrequency,
                     Quantity = fb.Quantity,
                     AffectQuantity = fb.GrowthStages.First(gs => gs.Status == GrowthStageStatusEnum.Active).AffectedQuantity,
+                    CurrentQuantity = fb.GrowthStages.First(gs => gs.Status == GrowthStageStatusEnum.Active).Quantity,
+                    DeadQuantity = fb.DeadQuantity,
                     Cage = fb.Cage == null ? null : new CageModel
                     {
                         Id = fb.Cage.Id,
